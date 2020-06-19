da7fb0bb9344e07a65bc475b2c0359f1f85b16bc
Lauserbub <64130966+Lauserbub@users.noreply.github.com>
2020-06-19 22:00:10 +0200

    Aufgabe08

---
+++ Aufgabe08/script.js
@@ -0,0 +1,19 @@
+"use strict";
+var Aufgabe08;
+(function (Aufgabe08) {
+    let signIn = document.getElementById("signIn");
+    signIn.addEventListener("click", communicate);
+    async function communicate() {
+        let formData = new FormData(document.forms[0]);
+        let url = "";
+        let query = new URLSearchParams(formData);
+        url = url + "?" + query.toString();
+        await fetch(url);
+        for (let entry of query) {
+            console.log(entry);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
+    }
+})(Aufgabe08 || (Aufgabe08 = {}));
+//# sourceMappingURL=script.js.map
---
+++ Aufgabe08/script.ts
@@ -0,0 +1,17 @@
+namespace Aufgabe08 {
+    let signIn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("signIn");
+    signIn.addEventListener("click", communicate);
+
+    async function communicate(): Promise<void> {
+        let formData: FormData = new FormData(document.forms[0]);
+        let url: string = "";
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url = url + "?" + query.toString();
+        await fetch(url);
+        for (let entry of query) {
+            console.log(entry);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
+    }
+}
