f99d20ce0e39441ac98b50c5537054fdb291adef
Lauserbub <64130966+Lauserbub@users.noreply.github.com>
2020-06-23 21:33:15 +0200

    Pray for 8

--- Aufgabe08/server.js
+++ Aufgabe08/server.js
@@ -4,22 +4,31 @@ exports.A08Server = void 0;
 const Http = require("http");
 var A08Server;
 (function (A08Server) {
+    //Konsolenausgabe
     console.log("Starting server");
+    //port Variable von Typ Number erstellt und zugewiesen
     let port = Number(process.env.PORT);
     if (!port)
         port = 8100;
     let server = Http.createServer();
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
+    //Server hört dem Port zu für Anfragen
     server.listen(port);
+    //Konsolenausgabe
     function handleListen() {
         console.log("Listening");
     }
     function handleRequest(_request, _response) {
+        //Konsolenausgabe
         console.log("I hear voices!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
+        //Ausgabe URL
         _response.write(_request.url);
+        //Konsolenausgabe
+        console.log(_request.url);
+        //Response Ende
         _response.end();
     }
 })(A08Server = exports.A08Server || (exports.A08Server = {}));
--- Aufgabe08/server.ts
+++ Aufgabe08/server.ts
@@ -1,7 +1,9 @@
 import * as Http from "http";
 
 export namespace A08Server {
+    //Konsolenausgabe
   console.log("Starting server");
+  //port Variable von Typ Number erstellt und zugewiesen
   let port: number = Number(process.env.PORT);
   if (!port)
     port = 8100;
@@ -9,20 +11,28 @@ export namespace A08Server {
   let server: Http.Server = Http.createServer();
   server.addListener("request", handleRequest);
   server.addListener("listening", handleListen);
+  //Server hört dem Port zu für Anfragen
   server.listen(port);
 
+  //Konsolenausgabe
   function handleListen(): void {
     console.log("Listening");
   }
 
   function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+    //Konsolenausgabe
     console.log("I hear voices!");
 
     _response.setHeader("content-type", "text/html; charset=utf-8");
     _response.setHeader("Access-Control-Allow-Origin", "*");
 
+    //Ausgabe URL
     _response.write(_request.url);
 
+    //Konsolenausgabe
+    console.log(_request.url);
+
+    //Response Ende
     _response.end();
   }
 }
